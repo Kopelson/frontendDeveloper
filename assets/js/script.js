6b3725f6b9ff56febba57ec4565e7ec37f179abb
Kenneth Kopelson <kennethkopelson@gmail.com>
2020-10-22 11:45:05 -0700

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
+//This adds a click handler that removes the expanded list on the navbar when the user clicks a list item.
 $(".li-selector").on("click", function(){
+   //assigns button a header-button id selector
    let button = $("#header-button");
+   //assigns navHandler a navbarSupportedContent id selector
    let navHandler = $("#navbarSupportedContent");
+   //this changes the aria-expanded attribute on button to false
    button.attr("aria-expanded", "false");
+   //this removes the class show from the navHandler
    navHandler.removeClass("show");
 })
