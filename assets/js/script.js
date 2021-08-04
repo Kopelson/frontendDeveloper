8a0defbf8985a7b37335bab6a419db8b1016dac8
Kenneth Kopelson <kennethkopelson@Kenneths-MacBook-Air.local>
2021-08-04 15:02:57 -0700

    allowed tabs to be all open on arrival, add css, fixed readMe, and added a script to show all if needed in the future, also removed content to build layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,4 +11,14 @@ $(".learn-more").on("click", function(){
    aboutTab = $('#nav-about-tab');
    aboutTab.trigger('click');
 });
+
+//shows all tabs
+$("#show_all").on("click", function() {
+   $(this).addClass("active").parent("li").siblings().find("a").removeClass("active");
+   $(".tab-pane").removeClass("fade").addClass("active").addClass("show");
+ });
+ $(".nav-link").not("#show_all").on("click", function() {
+   console.log(this.hash);
+   $(".tab-pane").not(this.hash).removeClass("active").removeClass("show");
+ });
  
